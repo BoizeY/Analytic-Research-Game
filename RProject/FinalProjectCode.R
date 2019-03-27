dbc381fcaffd3ef65328bfc0f26c7b4d3abb397c
Daniel MacCormick <dmaccormick@hotmail.com>
2019-03-27 15:00:56 -0400

    Converted over to Mixed Designs, Implemented most of the Assumptions
    Removed the Two Way ANOVA code, now switched over to the Mixed Designs. 6/7 assumptions are written. The last one is sphericity, not sure how to do that yet

--- RProject/FinalProjectCode.R
+++ RProject/FinalProjectCode.R
@@ -7,6 +7,8 @@
 # It has 1 dependent variable (AvgErrorValue)
 # Our study is repeated measures but participants are split into multiple groups
 # The groups are counterbalanced using a 4x4 Latin Square
+# Our within subject variable is NotificationType
+# Our between subject variable is the Group
 # Therefore, the best test for us to perform is the Mixed ANOVA
 #---------------------------------------------------------------------------------------#
 
@@ -28,13 +30,19 @@ participantData = read.csv(filePath)
 # Our DV, AvgErrorValue, is a continous float so this assumption PASSES
 
 
-# 2b - Two IV consist of at least two related groups
-# The first IV is ParticipantGroup, which has 4 possibilities (A,B,C,D) so this PASSES
-# The second IV is NotificationType, which has 4 possibilities as well (NONE, ANIMATION, ICON, COLOUR) so this PASSES
+# 2b - Within subject factors should consist of at least two related groups
+# Our within subject factor is the NotificationType
+# NotificationType can be NONE, ICON, ANIMATION, or COLOUR which is at least two
 # Therefore, this assumption PASSES
 
 
-# 2c - No significant outliers
+# 2c - Between subject factors should consist of at least two related groups
+# Our between subject factor is the Group
+# Group can be A, B, C, or D which is at least two
+# Therefore, this assumption PASSES
+
+
+# 2d - No significant outliers in any group of within subject factors or between subject factors
 # We can visualize the outliers by creating a box and whisker plot
 if (!require(ggpubr)) install.packages(("ggpubr"))
 library(ggpubr)
@@ -43,7 +51,8 @@ print(ggplot(participantData, aes(x = NotificationType, y = AvgErrorValue, fill 
 # None of the box and whisker plots show any outliers so this assumption PASSES
 
 
-# 2d - DV In Each Combination of Related Groups is Approximately Normally Distributed
+# 2e - DV in each combination of related groups is approximately normally distributed
+# We can check normality using the Shapiro-Wilk test
 for (ParticipantGroup in levels(participantData$ParticipantGroup))
 {
   for (NotificationType in levels(participantData$NotificationType))
@@ -59,13 +68,23 @@ for (ParticipantGroup in levels(participantData$ParticipantGroup))
     cat("\n\n")
   }
 }
-# Results: Group A~Animation are not normally distributed. All the others are
+# Results: 
+# Group A~Animation is not normally distributed. However, it has a p-value of 0.02 which is reasonably close to being normally distributed
+# All the others are confirmed normally distributed by the Shapiro-Wilk tests
+# This assumption PASSES, outside of the one p-value. After conferring with the prof, it is confirmed to be PASSED
 
 
-# 2e - No violations of spericity in all combinations of groups
-# EzANOVA performs the evaluation of the spericity so we will run that next
-if (!require(ez)) install.packages("ez")
-library(ez)
-ezModel = ezANOVA(data = participantData, dv = AvgErrorValue, wid=ParticipantID, between=.(ParticipantGroup, NotificationType), detailed = T, type = 3)
-print(ezModel)
+# 2f - There needs to be homogeneity of variance for each combination
+# We can check this with the Levene's test
+if (!require(car)) install.packages("car")
+library(car)
+print(leveneTest(AvgErrorValue~ParticipantGroup, data = participantData))
+print(leveneTest(AvgErrorValue~ParticipantGroup * NotificationType, data = participantData))
+# Results:
+# Test 1 -> p-value = 0.9454, so this PASSES
+# Test 2 -> p-value = 0.9895, so this PASSES
+# Since both Levene's tests have pssed, this assumption PASSES
 
+
+# 2g - Sphericity must be equal
+# We can confirm this with Mauchly's test
